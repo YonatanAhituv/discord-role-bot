786e965fe2487b96f9bdbf4da141744171b1edee
AtomicCoding <atomicgaming4life@gmail.com>
2019-08-19 20:10:43 -0700

    Ported to new discord.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import asyncio
 import json
 from time import sleep
 
@@ -9,51 +10,30 @@ token = data["token"]
 
 global targetChannel
 
+global messageauthor
+
+global checkedValue
+
+global custom
+
 matchmaking = False
 
 class MyClient(discord.Client):
 
-    async def removebotroles(self, member: discord.Member):
-        print(str(member))
-        roles = []
-        print("Clearing all bot assigned roles...")
-        for items in data["questions"]:
-            print(str(items))
-            try:
-                if data["questions"][items]["roles"] == 0:
-                    rolelist = data["questions"][items]["answers"]
-                else:
-                    rolelist = data["questions"][items]["roles"]
-                for item in rolelist:
-                    if item != "":
-                        roles.append(item)
-            except KeyError:
-                pass
-        print(str(roles))
-        print("Got list of all roles, and removing them from member now...")
-        rolestodelete = []
-        for role in member.roles:
-            if role.name in rolelist:
-                rolestodelete.append(role)
-        for role in rolestodelete:
-            await client.remove_roles(member, role)
-        print("Done!")
-
     async def mute(self, member: discord.Member):
         global matchmaking
         matchmaking = True
-        role = discord.utils.get(member.server.roles, name=data["newrole"])
+        role = discord.utils.get(member.guild.roles, name=data["newrole"])
         try:
-            await client.add_roles(member, role)
-            await self.removebotroles(member)
+            await member.edit(roles=[role])
         except:
             matchmaking = False
 
     async def unmute(self, member: discord.Member):
         global matchmaking
         matchmaking = False
-        role = discord.utils.get(member.server.roles, name=data["newrole"])
-        await client.remove_roles(member, role)
+        role = discord.utils.get(member.guild.roles, name=data["newrole"])
+        await member.remove_roles(role)
 
     async def on_ready(self):
         print()
@@ -65,12 +45,7 @@ class MyClient(discord.Client):
         print()
         global matchmaking
         matchmaking = False
-        # Do not run this unless you are 1000% sure what you are doing
-        # servers = list(client.servers)
-        # for server in servers:
-        #     for member in server.members:
-        #         await self.removebotroles(member)
-        for server in client.servers:
+        for server in client.guilds:
             for channel in server.channels:
                 if channel.name == data["targetchannels"]["welcome"]:
                     global targetChannel
@@ -109,7 +84,10 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         global matchmaking
         highestlen = 0
-        msglist = ["From: " + str(message.author), "Sent in: " + str(message.author.server) + " in #" + str(message.channel), "Content: " + str(message.content)]
+        try:
+            msglist = ["From: " + str(message.author), "Sent in: " + str(message.author.guild) + " in #" + str(message.channel), "Content: " + str(message.content)]
+        except AttributeError:
+            msglist = ["From: " + str(message.author), "In a " + str(message.channel), "Content: " + str(message.content)]
         for msg in msglist:
             if len(msg) > highestlen:
                 highestlen = len(msg)
@@ -118,14 +96,16 @@ class MyClient(discord.Client):
             print(msg)
         print("-" * highestlen)
         if message.content == "!matchmake":
+            await message.delete()
             if not matchmaking:
-                await client.delete_message(message)
-                statusmsg = await client.send_message(message.channel, "Matchmaking " + message.author.mention + "...")
+                statusmsg = await message.channel.send(content="Matchmaking " + message.author.mention + "...")
                 await self.matchmake(message.author)
-                await client.delete_message(statusmsg)
+                await statusmsg.delete()
             else:
                 em = discord.Embed(title="ERROR", description="\nAlready matchmaking someone else, please try again soon!")
-                await client.send_message(message.channel, embed=em)
+                statusmsg = await message.channel.send(embed=em)
+                sleep(2)
+                await statusmsg.delete()
         elif message.content == "!roleassign":
             await self.roleassign(message=message)
         else:
@@ -135,13 +115,13 @@ class MyClient(discord.Client):
 
     async def complain(self, message):
         complaint = message.content.replace("!complain ", "")
-        await client.delete_message(message)
+        await message.delete()
         em = discord.Embed(title="Success", description="\nComplaint has been sent to moderators!")
-        msg = await client.send_message(message.channel, embed=em)
+        msg = await message.channel.send(embed=em)
         em = discord.Embed(title="Complaint Recieved", description="\n" + str(complaint))
-        await client.send_message(complaintsChannel, embed=em)
+        await complaintsChannel.send(embed=em)
         sleep(2)
-        await client.delete_message(msg)
+        await msg.delete()
 
     async def roleassign(self, member=None, message=None):
         if message is not None:
@@ -151,127 +131,174 @@ class MyClient(discord.Client):
         print(membername)
         if membername in data["assignedroles"].keys():
             for role in data["assignedroles"][membername]:
-                role = discord.utils.get(member.server.roles, name=role)
-                await client.add_roles(member, role)
+                role = discord.utils.get(member.guild.roles, name=role)
+                await member.add_roles(role)
             if message is not None:
                 print("Assigning roles...")
                 rolelist = data["assignedroles"][membername]
                 rolelist = await self.clean_list(rolelist)
                 em = discord.Embed(title="Success", description="\nAssigned the following roles: " + rolelist + ".")
-                await client.send_message(message.channel, embed=em)
+                await message.channel.send(embed=em)
         else:
             if message is not None:
-                await client.send_message(message.channel, "Failed to assign roles, please contact " + "<@" + data["admintoken"] + "> to fix this.")
+                await message.channel.send("Failed to assign roles, please contact " + "<@" + data["admintoken"] + "> to fix this.")
+
+    def emojicheck(self, reaction, user):
+        print("Emoji check on")
+        global messageauthor
+        global checkedValue
+        global custom
+        if not custom:
+            return messageauthor == user and str(reaction.emoji) in checkedValue
+        else:
+            for item in checkedValue:
+                if str(reaction.emoji) == str(item):
+                    return messageauthor == user
+            return False
+
+    def textcheck(self, message):
+        global messageauthor
+        global targetChannel
+        return messageauthor == message.author and str(message.channel) == str(targetChannel)
 
     async def matchmake(self, member):
-        print(str(member))
+        print("Matchmaking: " + str(member))
         global matchmaking
+        global messageauthor
+        global checkedValue
+        global custom
         if matchmaking:
             em = discord.Embed(title="ERROR", description="\nAlready matchmaking someone else, please try again soon!")
-            await client.send_message(member, embed=em)
-            await client.send_message(member, data["serverinvite"])
-            await client.kick(member)
+            try:
+                await member.send(embed=em)
+                await member.send(data["serverinvite"])
+            except discord.errors.Forbidden:
+                pass
+            await member.kick()
             return
         if not member.bot:
             totalquestions = len(data["questions"].keys())
             await self.mute(member)
             if not matchmaking:
                 return
-            welcomemsg = await client.send_message(targetChannel, member.mention + ", welcome to " + data["servername"] + ". In order to begin, please answer the following questions.")
+            welcomemsg = await targetChannel.send(content=member.mention + ", welcome to " + data["servername"] + ". In order to begin, please answer the following questions.")
             i = 1
             for name in data["questions"]:
+                custom = False
                 additions = ""
                 if data["questions"][name]["reactiontype"] == "text":
                     optionlist = data["questions"][name]["answers"]
                     optionlist = await self.clean_list(optionlist, newlines=True)
                     additions = "\n\nPlease reply to the message with one of these options:\n" + optionlist
                 em = discord.Embed(title="Question " + str(i) + "/" + str(totalquestions) + ":", description="\n" + data["questions"][name]["question"] + additions)
-                msg = await client.send_message(targetChannel, embed=em)
+                msg = await targetChannel.send(embed=em)
                 if data["questions"][name]["reactiontype"] == "text":
                     while True:
                         print("Getting user input...")
-                        role = discord.utils.get(member.server.roles, name=data["textrole"])
-                        await client.add_roles(member, role)
-                        usermsg = await client.wait_for_message(author=member, timeout=120)
+                        role = discord.utils.get(member.guild.roles, name=data["textrole"])
+                        await member.add_roles(role)
+                        messageauthor = member
+                        try:
+                            usermsg = await client.wait_for('message', check=self.textcheck, timeout=120)
+                        except asyncio.TimeoutError:
+                            usermsg = None
                         # print(str(usermsg.content))
                         if usermsg is None:
                             print("Timed out")
                             matchmaking = False
-                            await client.delete_message(welcomemsg)
-                            await client.kick(member)
-                            await client.send_message(member, "Timed out, rejoin " + data["servername"] + " to try again.")
-                            await client.send_message(member, data["serverinvite"])
+                            await welcomemsg.delete()
+                            try:
+                                await member.send("Timed out, rejoin " + data["servername"] + " to try again.")
+                                await member.send(data["serverinvite"])
+                            except discord.errors.Forbidden:
+                                pass
+                            await member.kick()
                             break
                         msgcontent = usermsg.content
                         loweranswers = []
                         for item in data["questions"][name]["answers"]:
                             loweranswers.append(item.lower())
                         if msgcontent.lower() in loweranswers:
-                            role = discord.utils.get(member.server.roles, name=data["textrole"])
-                            await client.remove_roles(member, role)
+                            role = discord.utils.get(member.guild.roles, name=data["textrole"])
+                            await member.remove_roles(role)
                             if data["questions"][name]["roles"] == 0:
                                 answerIndex = loweranswers.index(msgcontent.lower())
                                 print("Roles is set to 0")
-                                role = discord.utils.get(member.server.roles, name=data["questions"][name]["answers"][answerIndex])
-                                await client.add_roles(member, role)
+                                role = discord.utils.get(member.guild.roles, name=data["questions"][name]["answers"][answerIndex])
+                                await member.add_roles(role)
                                 print("Role assigned")
                                 break
                             else:
                                 answerIndex = loweranswers.index(msgcontent)
-                                role = discord.utils.get(member.server.roles, name=data["questions"][name]["roles"][answerIndex])
-                                await client.add_roles(member, role)
+                                role = discord.utils.get(member.guild.roles, name=data["questions"][name]["roles"][answerIndex])
+                                await member.add_roles(role)
                                 break
-
-                    await client.delete_message(usermsg)
-                    await client.delete_message(msg)
-                    if msg is None:
+                    await msg.delete()
+                    if usermsg is None:
+                        msg = usermsg
                         break
+                    await usermsg.delete()
                     i += 1
                     continue
                 while True:
+                    print(str(client.emojis))
                     if data["questions"][name]["reactiontype"] == "custom":
+                        custom = True
                         emojis = []
                         for emoji in data["questions"][name]["answers"]:
-                            emojis.append(discord.utils.get(client.get_all_emojis(), name=emoji))
+                            emojis.append(discord.utils.get(client.emojis, name=emoji))
+                            print(str(emojis))
                     else:
                         emojis = data["questions"][name]["answers"]
                     for emoji in emojis:
-                        await client.add_reaction(msg, emoji)
-                    res = await client.wait_for_reaction(emojis, user=member, message=msg, timeout=120)
+                        await msg.add_reaction(emoji)
+                    print("Emojis list: " + str(emojis))
+                    messageauthor = member
+                    checkedValue = emojis
+                    try:
+                        res = await client.wait_for('reaction_add', check=self.emojicheck, timeout=120)
+                    except asyncio.TimeoutError:
+                        res = None
                     if res is None:
+                        print("Timed out")
                         matchmaking = False
-                        await client.kick(member)
-                        await client.delete_message(msg)
-                        await client.delete_message(welcomemsg)
-                        await client.send_message(member, "Timed out, rejoin " + data["servername"] + " to try again.")
-                        await client.send_message(member, data["serverinvite"])
+                        await msg.delete()
+                        await welcomemsg.delete()
+                        try:
+                            await member.send("Timed out, rejoin " + data["servername"] + " to try again.")
+                            await member.send(data["serverinvite"])
+                        except discord.errors.Forbidden:
+                            pass
+                        await member.kick()
                         break
+                    res = res[0]
                     if data["questions"][name]["reactiontype"] == "custom":
-                        res = res.reaction.emoji.name
+                        res = res.emoji.name
                         res = res.lower()
                     else:
-                        res = res.reaction.emoji
+                        res = res.emoji
                     if res in data["questions"][name]["answers"]:
                         answerIndex = data["questions"][name]["answers"].index(res)
                         print("Emoji at index:", str(answerIndex))
                         break
-                if msg is None:
+                if res is None:
+                    msg = res
                     break
                 else:
                     if data["questions"][name]["type"] == "role" and data["questions"][name]["roles"][answerIndex] != "":
-                        role = discord.utils.get(member.server.roles, name=data["questions"][name]["roles"][answerIndex])
+                        role = discord.utils.get(member.guild.roles, name=data["questions"][name]["roles"][answerIndex])
                         print(str(role))
-                        await client.add_roles(member, role)
+                        await member.add_roles(role)
                     if data["questions"][name]["type"] == "action":
                         if data["questions"][name]["actions"][answerIndex] == "close":
-                            await client.delete_message(msg)
+                            await msg.delete()
                             break
-                await client.delete_message(msg)
+                await msg.delete()
                 i += 1
             if msg is not None:
                 await self.unmute(member)
-                await client.send_message(targetChannel, "Welcome to " + data["servername"] + ", " + member.mention + ".")
-                await client.delete_message(welcomemsg)
+                await targetChannel.send("Welcome to " + data["servername"] + ", " + member.mention + ".")
+                await welcomemsg.delete()
                 await self.roleassign(member=member)
 
     async def on_member_join(self, member):
